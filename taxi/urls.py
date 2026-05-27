478feab06df0529723c0962b03ebaef71895e7ea
Dmytro Shulha <dimonchik0218@gmail.com>
2026-05-27 15:25:55 +0300

    task taxi_service_authentication fix urls

--- taxi/urls.py
+++ taxi/urls.py
@@ -24,9 +24,6 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-
 ]
 
 app_name = "taxi"
